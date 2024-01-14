d02590a2fb17ce48d73b42c6a73c4dd98739f981
Umar <umarg1997@gmail.com>
2024-01-14 09:58:33 +0000

    Refactor webhooks.tsx: Remove unnecessary code and update PRODUCTS_UPDATE logic

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -17,31 +17,6 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       if (session) {
         await db.session.deleteMany({ where: { shop } });
       }
-
-      break;
-    case "PRODUCTS_UPDATE":
-      const shopifyId = payload.admin_graphql_api_id;
-      const product = await db.product.findFirst({
-        where: { shopifyId },
-      });
-
-      if (!product) {
-        await db.product.create({
-          data: {
-            shopifyId: payload.admin_graphql_api_id,
-            name: payload.title,
-          },
-        });
-      } else {
-        await db.product.update({
-          where: {},
-          data: {
-            shopifyId: payload.admin_graphql_api_id,
-            name: payload.title,
-          },
-        });
-      }
-
       break;
     case "CUSTOMERS_DATA_REQUEST":
     case "CUSTOMERS_REDACT":
